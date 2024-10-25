f5a5893abd836d89a79dc8866aa0d9104d6b3465
surzatine <life.thruout@aleeas.com>
2024-10-25 20:59:57 +0545

    'commit'

---
+++ github.py
@@ -0,0 +1,29 @@
+import os
+
+
+def cmd(parameter):
+    result = os.system(parameter)
+    print(result)
+
+
+
+
+# --- File Appending ---
+def append_to_file(filename, text_to_append):
+
+
+    try:
+        with open(filename, 'a') as file:
+            file.write(text_to_append)
+        print(f"Successfully appended to '{filename}'")
+    except FileNotFoundError:
+        print(f"Error: File '{filename}' not found.")
+
+# Example usage:
+os.chdir("C:\\xampp\\htdocs\\me_user\\birthday")
+cmd("ls")
+append_to_file("README.md", "  ")
+
+cmd("git add .")
+cmd("git commit -m 'commit'")
+cmd("git push")
